8d8ce51c32b2a7a4520b5106dcccddafee7e83d8
devsensa <person.sensa@gmail.com>
2022-06-22 02:19:42 +0300

    chore: remove example

--- packages/flate/example/app/android/app/src/main/kotlin/dev/bysensa/app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.bysensa.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- packages/flate/example/app/lib/screens.dart
+++
@@ -1,5 +0,0 @@
-import 'package:go_router/go_router.dart';
-
-export 'screens/splash/screen.dart';
-
-List<GoRoute> routes = [];
